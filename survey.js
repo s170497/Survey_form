2ab46ed3891f16d063a42b9949cbb7b74fb984db
SATYA SRI PITHANI <105268486+s170497@users.noreply.github.com>
2023-10-17 10:31:37 +0530

    sample commit

---
+++ survey.js
@@ -0,0 +1,12 @@
+
+function bodyload(){
+document.getElementById("fntxt").innerHTML=document.getElementById("fn").value;
+document.getElementById("lntxt").innerHTML=document.getElementById("ln").value;
+document.getElementById("datetxt").innerHTML=document.getElementById("date").value;
+document.getElementById("countrytxt").innerHTML=document.getElementById("country").value;
+document.getElementById("gendertxt").innerHTML=document.getElementById("gender").value;
+document.getElementById("Professiontxt").innerHTML=document.getElementById("profession").value;
+document.getElementById("Emailtxt").innerHTML=document.getElementById("email").value;
+document.getElementById("mobiletxt").innerHTML=document.getElementById("phno").value;
+
+}
